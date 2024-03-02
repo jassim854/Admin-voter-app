4a05ee30f950d2e2e2ffbcfef8427d7ccf5ad27a
Jasim2023 <jasim@azzurri.pk>
2024-03-02 10:01:04 -0800

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:admin_voter_app/view/auth_/signup_player_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -32,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SignupPlayerView()
     );
   }
 }
